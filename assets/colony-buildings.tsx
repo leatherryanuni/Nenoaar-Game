58562ddc98718cd721c70e7e60291fa05b2b2cd2
oscarl77 <oscarlaurens18@gmail.com>
2024-10-17 21:53:56 +0100

    Added map and tile assets

---
+++ assets/colony-buildings.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.11.0" name="colony-db32-buildings-ready" tilewidth="32" tileheight="32" tilecount="2600" columns="20">
+ <image source="colony-db32-buildings-ready.png" width="640" height="4160"/>
+</tileset>
---
+++ assets/colony-grounds.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.11.0" name="colony-db32-grounds-ready" tilewidth="32" tileheight="32" tilecount="1020" columns="20">
+ <image source="colony-grounds.png" width="640" height="1632"/>
+</tileset>
