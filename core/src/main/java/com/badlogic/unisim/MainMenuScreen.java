c033cf5f45077daf705b82d2dd3c91763bf08bbf
Archie L <archieleppard@gmail.com>
2024-10-18 12:09:46 +0100

    Created menu screen, and added a background image to it. Also added background image to assets under StartScreen.jpg

--- core/src/main/java/com/badlogic/unisim/MainMenuScreen.java
+++ core/src/main/java/com/badlogic/unisim/MainMenuScreen.java
@@ -2,6 +2,9 @@ package com.badlogic.unisim;
 
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Screen;
+import com.badlogic.gdx.graphics.OrthographicCamera;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.utils.ScreenUtils;
 
 /**
  * First screen of the application. Displayed after the application is created.
@@ -10,9 +13,17 @@ import com.badlogic.gdx.Screen;
 public class MainMenuScreen implements Screen {
     // Reference the main game class to communicate with main game manager.
     private final UniSimGame game;
+    OrthographicCamera camera;
+    Texture menuScreen;
 
     public MainMenuScreen(UniSimGame game) {
         this.game = game;
+
+        camera = new OrthographicCamera();
+        camera.setToOrtho(false, 800, 480);
+
+        // Rescales the original menu screen to screen size
+        menuScreen = new Texture("StartScreen.jpg");
     }
 
     @Override
@@ -23,6 +34,15 @@ public class MainMenuScreen implements Screen {
     @Override
     public void render(float delta) {
         // Draw your screen here. "delta" is the time since last render in seconds.
+        ScreenUtils.clear(0.5f, 0, 0, 1);
+
+        camera.update();
+        game.batch.setProjectionMatrix(camera.combined);
+
+        game.batch.begin();
+        game.batch.draw(menuScreen, 0, 0, 800, 480);
+        game.batch.end();
+
         if (Gdx.input.justTouched()) {
             // Switch to GameScreen when user clicks screen.
             game.setScreen(new GameScreen(game));
--- core/src/main/java/com/badlogic/unisim/UniSimGame.java
+++ core/src/main/java/com/badlogic/unisim/UniSimGame.java
@@ -1,16 +1,19 @@
 package com.badlogic.unisim;
 
 import com.badlogic.gdx.Game;
+import com.badlogic.gdx.graphics.g2d.BitmapFont;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 
 /** {@link com.badlogic.gdx.ApplicationListener} implementation shared by all platforms. */
 public class UniSimGame extends Game {
     public SpriteBatch batch;
+    public BitmapFont font;
 
     @Override
     public void create() {
         // Spritebatch initialised here so multiple screens can access it
         batch = new SpriteBatch();
+        font = new BitmapFont();
         // Set the initial screen
         setScreen(new MainMenuScreen(this));
     }
