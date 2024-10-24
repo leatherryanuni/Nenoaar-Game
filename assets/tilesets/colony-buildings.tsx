9b2e16f24b1c526b1ccae742e8050d9c3a73cd4b
oscarl77 <oscarlaurens18@gmail.com>
2024-10-24 17:02:11 +0100

    Organised maps and tilesets into separate folders

---
+++ assets/tilesets/colony-buildings.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.11.0" name="colony-buildings" tilewidth="32" tileheight="32" tilecount="2600" columns="20">
+ <image source="colony-buildings.png" width="640" height="4160"/>
+</tileset>
